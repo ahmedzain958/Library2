699c361478945885353744f4e653d17ec361b27f
ahmedmohamedzain <ahmed.zaineldin@vodafone.com>
2022-02-12 21:36:20 +0200

    error handling

---
+++ app/src/main/java/com/example/library2/Failure.kt
@@ -0,0 +1,15 @@
+package com.example.library2
+/*Failure is inability of the system to do the job that you wanted to do, it went wrong  */
+/*
+Programs can go wrong for many reasons
+ - invalid input (strings with invalid values , numbers out of range and unexpectedly null pointers)
+ -
+* */
+class Failure {
+}
+/*exceptions from O'reilly kotlin next generation for modern android apps https://learning.oreilly.com/library/view/learn-kotlin-for/9781484244678/html/476388_1_En_8_Chapter.xhtml */
+/**
+ * For programs with a higher level of complexity, those built by many developers, or those that use external programs (libraries), the situation is not that clear. Problems will arise, for example, if lists or arrays get addressed out of bounds, some I/O access to files or network data streams fails, or objects end up in an unanticipated or corrupted state.
+
+This is what exceptions are for. Exceptions are objects that get created or thrown if something unanticipated and possibly malicious happens. Special program parts can then receive such exception objects and act appropriately.
+ */
