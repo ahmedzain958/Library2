6d048a9f2aa5f04e9744920ab9eb268a41b4c89c
ahmedmohamedzain <ahmed.zaineldin@vodafone.com>
2022-02-12 20:53:00 +0200

    custom exception

--- app/src/main/java/com/example/library2/ExceptionHandling.kt
+++ app/src/main/java/com/example/library2/ExceptionHandling.kt
@@ -1,7 +1,5 @@
 package com.example.library2
 
-import java.lang.ArithmeticException
-
 /*exception is abnormal condition, code is successfully compiled and you can execute it, but while executing any event that interrupts the normal flow of your execution
 is called exception, happens in runtime
 
@@ -9,7 +7,30 @@ ArithmeticException, OutOfBoundException, ... are classes used for exception han
 You can create you own custom exception class, each exception class contains the message, stacktrace
  */
 fun main() {
+try {
+    vote2("Ahmed", 7)
+}catch (e: IllegalVoterException){
+    e.printStackTrace()
+}catch (e: java.lang.Exception){}//multiple catch but one finally
 //exception as expression
-    val result = try{10/0}catch (e: ArithmeticException){ 0 }
-    println(result)
+    val result = try {
+        println("inside try")
+        10 / 0
+    } catch (e: ArithmeticException) {
+        0
+    }
+    println("the result is $result")
 }
+
+//Exception classes hierarchy Throwable(master exception class) -> Exception -> predefined exceptions
+fun vote(name: String, age: Int) {
+    if (age < 18) throw IllegalArgumentException("Younger than 18 can't vote")
+    print("$name voted")
+}
+
+fun vote2(name: String, age: Int) {
+    if (age < 18) throw IllegalVoterException("Younger than 18 can't vote")
+    print("$name voted")
+}
+
+class IllegalVoterException(message: String) : Exception(message)
