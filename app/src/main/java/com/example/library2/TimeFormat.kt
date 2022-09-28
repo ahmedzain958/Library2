55a6ee597e12355bb47f92c2eabf8d821e883f12
Ahmed Zain <ahmedzain958@gmail.com>
2022-09-28 16:22:28 +0200

    current time yyyy-

---
+++ app/src/main/java/com/example/library2/TimeFormat.kt
@@ -0,0 +1,13 @@
+package com.example.library2
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun getCurrentTime():String {
+    val current = LocalDateTime.now()
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
+    return current.format(formatter)
+}
