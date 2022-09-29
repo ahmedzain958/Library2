108de94e4067110cf62b8b009396f05bb58d500d
Ahmed Zain <ahmedzain958@gmail.com>
2022-09-29 10:59:15 +0200

    2 parallel coroutines async and await

--- app/src/main/java/com/example/library2/RunBlockingActivity.kt
+++ app/src/main/java/com/example/library2/RunBlockingActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_run_blocking.view.*
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class RunBlockingActivity : AppCompatActivity() {
     lateinit var textView: TextView
@@ -14,7 +15,6 @@ class RunBlockingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_run_blocking)
         textView = findViewById<TextView>(R.id.textView)
-        Log.d("RunBlockingActivity", "run in:" + Thread.currentThread().name + " thread")
         GlobalScope.launch {
             launch {
                 printCoroutines("Zain")
@@ -24,6 +24,21 @@ class RunBlockingActivity : AppCompatActivity() {
             }
         }
         Log.d("RunBlockingActivity", "return to :" + Thread.currentThread().name + " thread")
+
+
+        GlobalScope.launch {
+            val millis = measureTimeMillis {
+                val networkUser = async { getUserFromNetwork() }
+                val databaseUser = async { getUserFromDatabase() }
+
+                if (networkUser.await() == databaseUser.await()) {
+                    Log.d("2RunBlockingActivity", "Equals")
+                } else {
+                    Log.d("2RunBlockingActivity", "Not Equals")
+                }
+            }
+            Log.d("2RunBlockingActivity", "secs=${millis/1000}")
+        }
     }
 
     suspend fun printCoroutines(text: String) {
@@ -32,5 +47,15 @@ class RunBlockingActivity : AppCompatActivity() {
             "RunBlockingActivity",
             "hello $text - thread:" + Thread.currentThread().name
         )
+    }
+
+    suspend fun getUserFromNetwork(): String {
+        delay(2000)
+        return "Zain"
+    }
+
+    suspend fun getUserFromDatabase(): String {
+        delay(2000)
+        return "Zain"
     }
 }
