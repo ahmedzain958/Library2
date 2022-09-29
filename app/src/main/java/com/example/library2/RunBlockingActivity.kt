6ee0b1b6cf5eb92c3bd54573b7e16eccb1651c5c
Ahmed Zain <ahmedzain958@gmail.com>
2022-09-29 05:30:02 +0200

    2 parallel coroutines

--- app/src/main/java/com/example/library2/RunBlockingActivity.kt
+++ app/src/main/java/com/example/library2/RunBlockingActivity.kt
@@ -9,26 +9,28 @@ import kotlinx.android.synthetic.main.activity_run_blocking.view.*
 import kotlinx.coroutines.*
 
 class RunBlockingActivity : AppCompatActivity() {
-    lateinit var textView:TextView
+    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_run_blocking)
         textView = findViewById<TextView>(R.id.textView)
         Log.d("RunBlockingActivity", "run in:" + Thread.currentThread().name + " thread")
-        runBlocking {
-            printCoroutines()
-            textView.text = "Zain"
-            Log.d("RunBlockingActivity", "current time :" + Thread.currentThread().name + " thread")
+        GlobalScope.launch {
+            launch {
+                printCoroutines("Zain")
+            }
+            launch {
+                printCoroutines("Zain2")
+            }
         }
         Log.d("RunBlockingActivity", "return to :" + Thread.currentThread().name + " thread")
     }
 
-    suspend fun printCoroutines() {
+    suspend fun printCoroutines(text: String) {
         delay(2000)
         Log.d(
             "RunBlockingActivity",
-            "hello from inside coroutines - thread:" + Thread.currentThread().name
+            "hello $text - thread:" + Thread.currentThread().name
         )
     }
-
 }
