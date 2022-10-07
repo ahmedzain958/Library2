f9e9b6f90fe1e39303645bf0cef3525e4ea2213e
Ahmed Zain <ahmedzain958@gmail.com>
2022-10-07 06:37:50 +0200

    service binding

---
+++ app/src/main/java/com/example/library2/longrunningboundservice/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.library2.longrunningboundservice
+
+const val LOG_TAG = "CodeRunner"
+const val FILE_URL = "https://2833069.youcanlearnit.net/lorem_ipsum.txt"
---
+++ app/src/main/java/com/example/library2/longrunningboundservice/LongRunningBoundServiceMainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.library2.longrunningboundservice
+
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.util.Log
+import android.widget.ScrollView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.library2.databinding.ActivityMainRunnableObjectBinding
+
+class LongRunningBoundServiceMainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainRunnableObjectBinding
+    private lateinit var myservice: MyService
+    private val serviceConnection = object: ServiceConnection{
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as MyService.MyServiceBinder
+            myservice = binder.getService()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {}
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initialize view binding for view object references
+        binding = ActivityMainRunnableObjectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize button click handlers
+        with(binding) {
+            runButton.setOnClickListener { runCode() }
+            clearButton.setOnClickListener { clearOutput() }
+        }
+
+    }
+
+    /**
+     * Run some code
+     */
+    private fun runCode() {
+
+    }
+
+    /**
+     * Clear log display
+     */
+    private fun clearOutput() {
+        binding.logDisplay.text = ""
+        scrollTextToEnd()
+    }
+
+    /**
+     * Log output to logcat and the screen
+     */
+    @Suppress("SameParameterValue")
+    private fun log(message: String) {
+        Log.i(LOG_TAG, message)
+        binding.logDisplay.append(message + "\n")
+        scrollTextToEnd()
+    }
+
+    /**
+     * Scroll to end. Wrapped in post() function so it's the last thing to happen
+     */
+    private fun scrollTextToEnd() {
+        Handler().post { binding.scrollView.fullScroll(ScrollView.FOCUS_DOWN) }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Intent(this, MyService::class.java).also {
+
+        }
+    }
+}
