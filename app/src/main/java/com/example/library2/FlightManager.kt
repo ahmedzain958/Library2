e87e1786bb8eee212312d242c31872dba1533ea4
ahmedmohamedzain <ahmed.zaineldin@vodafone.com>
2022-10-11 16:18:11 +0200

    devskiller mid

--- app/src/main/java/com/example/library2/FlightManager.kt
+++ app/src/main/java/com/example/library2/FlightManager.kt
@@ -3,12 +3,27 @@ package com.example.library2
 data class Flight(val flightNumber: String, val origin: String, val destination: String)
 
 class FlightManager {
+    val listOfFlights = mutableListOf<Flight>()
 
     fun addNewFlight(flight: Flight): Boolean {
-       return true
+        val hasFlight: Boolean = listOfFlights.any {
+            it.flightNumber == flight.flightNumber
+        }
+        if (!hasFlight) {
+            listOfFlights.add(flight)
+            return true
+        }
+        return false
     }
 
-    fun findFlightsBetween(origin: String, destination: String, directionSensitive: Boolean): List<Flight> {
-        return emptyList()
+    fun findFlightsBetween(
+        origin: String,
+        destination: String,
+        directionSensitive: Boolean,
+    ): List<Flight> {
+        val flights = listOfFlights.filter {
+            if (directionSensitive) it.origin == origin && it.destination == destination else it.origin == origin
+        }
+        return flights
     }
 }
