6b9f0c4b9ba4213c697e0626bc92cd9b684493c6
ahmedmohamedzain <ahmed.zaineldin@vodafone.com>
2022-10-16 20:17:26 +0200

    main list + views activity +lottie animation using activity and fragment

--- app/src/main/java/com/example/library2/views/animatedviews/lottie/LottieViewActivity.kt
+++ app/src/main/java/com/example/library2/views/animatedviews/lottie/LottieViewActivity.kt
@@ -10,6 +10,8 @@ class LottieViewActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lottie_view)
+        val mainFragment = LottieButtonFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.activity_lottie, mainFragment).commit()
     }
-
 }
