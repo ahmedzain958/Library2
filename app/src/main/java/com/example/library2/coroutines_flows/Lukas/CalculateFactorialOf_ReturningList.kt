95d936a0d6927c4a13a3b4e76951f582a1f2bfe6
Zain <ahmed.zaineldin@vodafone.com>
2023-08-19 21:02:01 +0300

    مهسف

--- app/src/main/java/com/example/library2/coroutines_flows/Lukas/CalculateFactorialOf_ReturningList.kt
+++ app/src/main/java/com/example/library2/coroutines_flows/Lukas/CalculateFactorialOf_ReturningList.kt
@@ -1,10 +1,13 @@
+/*
 package com.example.library2.coroutines_flows.Lukas
 
+import com.example.library2.utils.printWithTimePassed
 import java.math.BigInteger
 
 fun main() {
-    val result = calculateFactorialOf(5)
-    println("Result: $result")
+   calculateFactorialOf(5).forEach{
+        printWithTimePassed(it, startTime = System.currentTimeMillis())
+    }
 }
 
 private fun calculateFactorialOf(number: Int):List<BigInteger> =  buildList {
@@ -14,4 +17,4 @@ private fun calculateFactorialOf(number: Int):List<BigInteger> =  buildList {
         factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
     }
     add(factorial) // to build a list we need add with no need for return
-}
+}*/
