df9073dcb872320c6e3673332eabe65939ef719c
Zain <ahmed.zaineldin@vodafone.com>
2023-09-11 18:09:24 +0300

    List(10)

--- app/src/main/java/com/example/library2/kotlinfns/ListDuplicated.kt
+++ app/src/main/java/com/example/library2/kotlinfns/ListDuplicated.kt
@@ -1,7 +1,7 @@
 package com.example.library2.kotlinfns
 
 fun main() {
-   val names: List<String> = List(10) { "$it" }
+   val names: List<String> = List(30, { "$it" })
     names.forEach {
         println(it)
     }
