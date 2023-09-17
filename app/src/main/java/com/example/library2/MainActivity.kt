87fa06415505dc65869a151c2a9b2576c27344d5
Zain <ahmed.zaineldin@vodafone.com>
2023-09-17 15:43:35 +0300

    all compose dependencies setup

--- app/src/main/java/com/example/library2/MainActivity.kt
+++ app/src/main/java/com/example/library2/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val items = listOf(
             "Views" to ViewsActivity::class.java,
             "Concurrent Programming" to ConcurrentProgrammingActivity::class.java,
