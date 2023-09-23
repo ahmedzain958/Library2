63c288c145cd30effa7ad10474a8d16169cc259f
Zain <ahmed.zaineldin@vodafone.com>
2023-09-23 22:02:29 +0300

    hilt dep.

---
+++ app/src/main/java/com/example/library2/ApplicationClass.kt
@@ -0,0 +1,8 @@
+package com.example.library2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ApplicationClass : Application() {
+}
--- app/src/main/java/com/example/library2/MainActivity.kt
+++ app/src/main/java/com/example/library2/MainActivity.kt
@@ -14,6 +14,7 @@ import com.example.library2.googlelibraries.RecaptchaGoogleActivity
 import com.example.library2.googlelibraries.SafetyNetRecaptchaActivity
 import com.example.library2.oop.OOPActivity
 import com.example.library2.views.ViewsActivity
+import dagger.hilt.EntryPoint
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
