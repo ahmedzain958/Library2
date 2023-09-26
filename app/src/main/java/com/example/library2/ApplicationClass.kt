9876bdb7596d1c2fb14d9c0a2e2cef372003bbd1
Zain <ahmed.zaineldin@vodafone.com>
2023-09-26 23:17:33 +0300

    dagger

--- app/src/main/java/com/example/library2/ApplicationClass.kt
+++ app/src/main/java/com/example/library2/ApplicationClass.kt
@@ -4,5 +4,9 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
+/**
+ * It generates the top level application component (App Component)
+ */
 class ApplicationClass : Application() {
+
 }
---
+++ app/src/main/java/com/example/library2/compose/mitch_recipe/di/AppModule.kt
@@ -0,0 +1,5 @@
+package com.example.library2.compose.mitch_recipe.di
+
+object AppModule  {
+
+}
