85283b8ff4ae97893a2cf64ce5a7584cfabf9d95
Zain <ahmed.zaineldin@vodafone.com>
2023-09-28 17:04:25 +0300

    fix hilt dependencies

--- app/src/main/java/com/example/library2/compose/mitch_recipe/di/AppModule.kt
+++ app/src/main/java/com/example/library2/compose/mitch_recipe/di/AppModule.kt
@@ -1,5 +1,11 @@
 package com.example.library2.compose.mitch_recipe.di
 
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule  {
 
 }
---
+++ app/src/main/java/com/example/library2/compose/mitch_recipe/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.library2.compose.mitch_recipe.presentation.ui.recipe_list
+
+import androidx.lifecycle.ViewModel
+
+class RecipeListViewModel constructor(): ViewModel(){
+
+}
