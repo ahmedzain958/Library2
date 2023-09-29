919bfcffe855ba971d3c985dc92f25ee456fa17c
Zain <ahmed.zaineldin@vodafone.com>
2023-09-29 23:55:53 +0300

    hilt vs koin

--- app/src/main/java/com/example/library2/compose/mitch_recipe/di/AppModule.kt
+++ app/src/main/java/com/example/library2/compose/mitch_recipe/di/AppModule.kt
@@ -1,14 +1,21 @@
 package com.example.library2.compose.mitch_recipe.di
 
+import android.content.Context
+import com.example.library2.ApplicationClass
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 /**
  * Our dependencies will exist as long as our application is alive
  */
-object AppModule  {
-
+object AppModule {
+    @Singleton
+    fun provideApplication(/*@ApplicationContext */app: Context): ApplicationClass {
+        return app as ApplicationClass
+    }
 }
---
+++ app/src/main/java/com/example/library2/compose/mitch_recipe/presentation/ui/recipe/RecipeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.library2.compose.mitch_recipe.presentation.ui.recipe
+
+import androidx.lifecycle.ViewModel
+
+class RecipeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
